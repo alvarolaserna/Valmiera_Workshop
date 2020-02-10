72ce1728bfcb20304ede24cb85222b910f237651
Álvaro Santos Laserna López <alvaro.lasernalopez@testdevlab.com>
2020-02-10 16:04:00 +0200

    new changes

--- src/test/java/stepDefinitions/LandingSteps.java
+++ src/test/java/stepDefinitions/LandingSteps.java
@@ -4,36 +4,41 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import screens.LandingScreen;
 import testUI.Configuration;
 
 import static testUI.UIOpen.open;
 import static testUI.Utils.AppiumHelps.sleep;
 
-public class LandingScreen {
+public class LandingSteps {
+    private LandingScreen landingScreen = new LandingScreen();
+
     @Given("I open the application")
     public void iOpenTheApplication() {
+        Configuration.useAllure = true;
         Configuration.automationType = Configuration.ANDROID_PLATFORM;
         Configuration.androidAppPath = "src/test/resources/groceries-app.apk";
         open();
-        sleep(3000);
     }
 
     @When("I click on add shopping list")
     public void iClickOnAddShoppingList() {
-        
+        landingScreen.clickAddShoppingList();
     }
 
     @And("I type {string}")
     public void iType(String arg0) {
-        
+        landingScreen.typeInShoppingName(arg0);
     }
 
     @And("I click on add list")
     public void iClickOnAddList() {
-        
+        landingScreen.clickOnAddList();
     }
 
-    @Then("I see {string} on landing screen")
+    @Then("I see new list {string} on landing screen")
     public void iSeeOnLandingScreen(String arg0) {
+        landingScreen.checkNewList(arg0);
+        sleep(3000);
     }
 }
--- target/test-classes/features/simple.feature
+++ target/test-classes/features/simple.feature
@@ -8,4 +8,4 @@ Feature: Shopping list
       When I click on add shopping list
       And I type "First List"
       And I click on add list
-      Then I see "First List" on landing screen
+      Then I see new list "First List" on landing screen
